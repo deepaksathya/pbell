97ec184e186059eb7b3be9c318ab1d9c8ccbe08c
Gene Super <gene@renttherunway.com>
2012-10-17 15:25:16 -0400

    Initial rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pbell::Application.config.secret_token = '8a3ff7fbd0e32fbcd2589b1534956f4e00f5497b0805560f4b277936ede03797ea26af2c6a562655a6531b5c2959f0c0d6d388c0d3083140d4285629f8abbf0d'
